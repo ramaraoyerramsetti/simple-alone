c5f6d529d6b550515c55e420331c777f115e3f02
ramaraoyerramsetti <ram.yerram99@gmail.com>
2013-04-07 13:04:06 +0530

    Initial Commit

---
+++ src/main/java/com/acc/TestApp.java
@@ -0,0 +1,12 @@
+package com.acc;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Name : Ramarao Yerramsetti");
+		System.out.println("Project : First alone...Maven");
+	}
+
+}
